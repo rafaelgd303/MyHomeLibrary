a5f4fb5c87f99ebe4096ac6514be35be222e34f2
Rafal Pawlowski <rafal.pawlowski11@gmail.com>
2020-05-15 17:33:35 +0200

    added functionality to the book repository

--- MyHomeLibrary/Repositories/BooksRepository.cs
+++ MyHomeLibrary/Repositories/BooksRepository.cs
@@ -9,24 +9,43 @@ namespace MyHomeLibrary.Repositories
 {
     public class BooksRepository : IBooksInterface
     {
+        private List<Book> _books;
+        private int _counter = 2; 
+
+        public BooksRepository()
+        {
+            _books = new List<Book>
+            {
+                new Book {Id = 0, Title = "Hobbit", Author = "JRR Tolkien", Page = 290},
+                new Book {Id = 1, Title = "Lord of the Rings", Author = "JRR Tolkien", Page = 850}
+            };
+        }
+
         public void AddBook(Book book)
         {
-            throw new NotImplementedException();
+            book.Id = _counter;
+            _books.Add(book);
+            _counter++;
         }
 
         public void DeleteBook(int id)
         {
-            throw new NotImplementedException();
+            var bookToDelete = _books.SingleOrDefault(book => book.Id == id);
+            _books.Remove(bookToDelete);
         }
 
         public List<Book> GetBooks()
         {
-            throw new NotImplementedException();
+            return _books;
         }
 
         public void UpdateBook(Book book)
         {
-            throw new NotImplementedException();
+            int bookIndexToUpdate = _books.FindIndex(b => b.Id == book.Id);
+            if (bookIndexToUpdate != -1)
+            {
+                _books[bookIndexToUpdate] = book;
+            }
         }
     }
 }
